1f39c4011db55ff415c5c71f7cb4a70de8aea324
Artem Korobko <artem.korobko@sizmek.com>
2019-11-20 22:13:41 +0100

    Implement basic simple password generator

---
+++ src/algorythm/simple.rs
@@ -0,0 +1,19 @@
+use crate::options::password_length::PasswordLength;
+use rand::Rng;
+use crate::algorythm::generator::generate_random_lowercase_letter;
+
+const LENGTH_BOUNDS: usize = 2;
+
+pub fn generate_password(average_length: usize) -> String {
+    let mut thread_rng = rand::thread_rng();
+    let min_length = average_length - LENGTH_BOUNDS;
+    let max_length = average_length + LENGTH_BOUNDS;
+    let password_length = thread_rng.gen_range(min_length, max_length);
+    let mut password = String::with_capacity(password_length);
+
+    for _ in 0..password_length {
+        password.push(generate_random_lowercase_letter(&mut thread_rng));
+    }
+
+    password
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 use crate::options::options::Options;
+use crate::options::password_length::PasswordLength;
 
-mod generator;
+mod algorythm;
 mod options;
 
+const PASSWORD_AVERAGE_LENGTH_SHORT: usize = 8;
+const PASSWORD_AVERAGE_LENGTH_REGULAR: usize = 12;
+const PASSWORD_AVERAGE_LENGTH_LONG: usize = 16;
+
 fn main() {
-    Options::from_args();
+    let options= Options::from_args();
+
+    let password = match options.length {
+        PasswordLength::Short => generate_password(PASSWORD_AVERAGE_LENGTH_SHORT),
+        PasswordLength::Regular => generate_password(PASSWORD_AVERAGE_LENGTH_REGULAR),
+        PasswordLength::Long => generate_password(PASSWORD_AVERAGE_LENGTH_LONG),
+    };
+
+    println!("{}", password);
+}
+
+fn generate_password(average_length: usize) -> String {
+    algorythm::simple::generate_password(average_length)
 }
