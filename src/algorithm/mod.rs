d0f7d3a0da370b07d4eeacb95a328a9a1398fb6f
korobko <korobko@amazon.com>
2019-11-22 13:24:53 +0100

    Hide generator module

--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -1,8 +1,9 @@
+mod generator;
+mod initial_password;
+
 pub mod algorithm;
-pub mod generator;
 pub mod generator_lowercase;
 pub mod generator_number;
 pub mod generator_symbol;
 pub mod generator_tokenizer;
 pub mod generator_uppercase;
-mod initial_password;
