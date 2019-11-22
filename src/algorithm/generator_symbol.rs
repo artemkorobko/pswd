81b4dac8db2164c3e1b409c9d9927bba81ce3225
korobko <korobko@amazon.com>
2019-11-22 09:42:18 +0100

    Implement symbol generator

---
+++ src/algorithm/generator_symbol.rs
@@ -0,0 +1,34 @@
+use rand::Rng;
+use rand::prelude::ThreadRng;
+use crate::algorithm::generator::Generator;
+
+const SYMBOLS_TABLE: [char; 29] = [
+    '!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';',
+    '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'
+];
+
+#[derive(Debug)]
+pub struct GeneratorSymbol;
+
+impl Generator for GeneratorSymbol {
+    fn generate_character(&self, random: &mut ThreadRng) -> char {
+        let symbol_index = random.gen_range(0, SYMBOLS_TABLE.len());
+        SYMBOLS_TABLE[symbol_index]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::algorithm::generator_symbol::*;
+    use crate::algorithm::generator::Generator;
+
+    #[test]
+    fn should_generate_random_character_in_range() {
+        let mut random = rand::thread_rng();
+        let generator = GeneratorSymbol;
+
+        let result = generator.generate_character(&mut random);
+
+        assert!(SYMBOLS_TABLE.contains(&result));
+    }
+}
--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -2,6 +2,7 @@ pub mod algorithm;
 pub mod generator;
 pub mod generator_lowercase;
 pub mod generator_number;
+pub mod generator_symbol;
 pub mod generator_uppercase;
 pub mod simple;
 pub mod tokenized;
