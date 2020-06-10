30d8703720c998fb9bc8c6787b8d1a98c292f7a0
artem <artem.korobko@gmail.com>
2020-06-10 23:06:09 +0200

    Generate empty token with capacity by default

--- src/password/token.rs
+++ src/password/token.rs
@@ -2,3 +2,27 @@
 pub struct Token {
     value: String,
 }
+
+impl Token {
+    pub fn new(capacity: usize) -> Self {
+        Self {
+            value: String::with_capacity(capacity)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::password::token::Token;
+    use spectral::prelude::*;
+
+    const CAPACITY: usize = 10;
+
+    #[test]
+    fn should_generate_default_empty_token() {
+        let token = Token::new(CAPACITY);
+
+        assert_that(&token.value.len()).is_equal_to(0);
+        assert_that(&token.value.capacity()).is_equal_to(CAPACITY);
+    }
+}
