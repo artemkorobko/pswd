aed3984854c3ae95fea56f3fcd0a097e6f2ba2fd
Artem Korobko <artem.korobko@gmail.com>
2025-04-17 00:55:08 +0200

    Choose the random character from the pattern except specified.

--- src/pattern.rs
+++ src/pattern.rs
@@ -1,4 +1,5 @@
 use rand::{seq, Rng};
+use std::collections::HashSet;
 
 pub struct Pattern<'a>(&'a str);
 
@@ -7,8 +8,42 @@ impl<'a> Pattern<'a> {
         Self(s)
     }
 
-    fn choose_random_char<R: Rng>(&self, rng: &mut R) -> Option<char> {
+    pub fn choose_random_char<R: Rng>(&self, rng: &mut R) -> Option<char> {
         seq::IteratorRandom::choose(self.0.chars(), rng)
+    }
+
+    pub fn choose_random_char_except<R: Rng>(
+        &self,
+        rng: &mut R,
+        exception: char,
+        mut retries: usize,
+    ) -> Option<char> {
+        if !self.contains_only_uniques() {
+            return None;
+        }
+
+        while retries > 0 {
+            let chosen = self.choose_random_char(rng);
+            if chosen? != exception {
+                return chosen;
+            }
+
+            retries -= 1;
+        }
+
+        None
+    }
+
+    fn contains_only_uniques(&self) -> bool {
+        let mut symbols = HashSet::with_capacity(self.0.len());
+
+        for symbol in self.0.chars() {
+            if !symbols.insert(symbol) {
+                return false;
+            }
+        }
+
+        true
     }
 }
 
@@ -24,5 +59,37 @@ mod tests {
         let char = pattern.choose_random_char(&mut rng).unwrap();
 
         assert!(pattern.0.contains(char));
+    }
+
+    #[test]
+    fn do_not_choose_random_char_if_pattern_is_empty() {
+        let pattern = Pattern::from_str("");
+        let mut rng = rand::rng();
+
+        let char = pattern.choose_random_char(&mut rng);
+
+        assert!(char.is_none());
+    }
+
+    #[test]
+    fn choose_random_char_except() {
+        let pattern = Pattern::from_str("abc");
+        let mut rng = rand::rng();
+
+        for _ in 0..10 {
+            let char = pattern.choose_random_char_except(&mut rng, 'a', 10).unwrap();
+            assert_ne!(char, 'a');
+            assert!(pattern.0.contains(char));
+        }
+    }
+
+    #[test]
+    fn do_not_choose_random_char_except_if_pattern_is_not_unique() {
+        let pattern = Pattern::from_str("aabbcc");
+        let mut rng = rand::rng();
+
+        let char = pattern.choose_random_char_except(&mut rng, 'a', 3);
+
+        assert!(char.is_none());
     }
 }
